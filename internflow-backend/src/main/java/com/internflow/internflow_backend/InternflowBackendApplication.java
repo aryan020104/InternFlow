b4cbcb43cf551aa5ccd9a3f7a775d0a431825727
s240224 <s240224@th-ab.de>
2026-07-10 14:01:13 +0200

    first commit

---
+++ internflow-backend/src/main/java/com/internflow/internflow_backend/InternflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internflow.internflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternflowBackendApplication.class, args);
+	}
+
+}
---
+++ internflow-backend/src/test/java/com/internflow/internflow_backend/InternflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internflow.internflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
