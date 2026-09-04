c6aebb89dc0b0203264bfb912dda7a4713ddcd50
s240224 <s240224@th-ab.de>
2026-09-04 18:18:42 +0200

    user updates

---
+++ internflow-backend/src/main/java/com/internflow/internflow_backend/entity/Role.java
@@ -0,0 +1,7 @@
+package com.internflow.internflow_backend.entity;
+
+public enum  Role {
+    STUDENT,
+    COMPANY,
+    UNIVERSITY
+}
--- internflow-backend/src/main/java/com/internflow/internflow_backend/entity/User.java
+++ internflow-backend/src/main/java/com/internflow/internflow_backend/entity/User.java
@@ -1,5 +1,6 @@
 package com.internflow.internflow_backend.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,11 +8,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.persistence.PreUpdate;
 
 @Entity
 @Table(name = "users")
@@ -33,6 +38,45 @@ public class User {
     @OneToMany(mappedBy = "user")
     @JsonIgnore
     private List<Internship> internships;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }   
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
     public void setId(UUID id) {
         this.id = id;
@@ -78,10 +122,11 @@ public class User {
 
     }
 
-    public User(String fullname, String email, String password) {
+    public User(String fullname, String email, String password, Role role) {
         this.fullname = fullname;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
 
 }
