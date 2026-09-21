2f392a1a4d09d7f970da5c39d187f898de9f122e
s240224 <s240224@th-ab.de>
2026-09-21 20:17:10 +0200

    changes

--- internflow-backend/src/main/java/com/internflow/internflow_backend/service/UserService.java
+++ internflow-backend/src/main/java/com/internflow/internflow_backend/service/UserService.java
@@ -6,10 +6,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.internflow.internflow_backend.dto.AuthResponse;
+import com.internflow.internflow_backend.entity.Company;
 import com.internflow.internflow_backend.entity.Role;
 import com.internflow.internflow_backend.entity.User;
 import com.internflow.internflow_backend.exception.EmailAlreadyExistsException;
 import com.internflow.internflow_backend.exception.InvalidCredentialsException;
+import com.internflow.internflow_backend.repository.CompanyRepository;
 import com.internflow.internflow_backend.repository.UserRepository;
 import com.internflow.internflow_backend.security.JwtService;
 
@@ -19,11 +21,14 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final CompanyRepository companyRepository;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    public UserService(UserRepository userRepository, CompanyRepository companyRepository,
+            PasswordEncoder passwordEncoder, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
+        this.companyRepository = companyRepository;
     }
 
     public AuthResponse loginUser(String email, String password) {
@@ -36,15 +41,38 @@ public class UserService {
         return new AuthResponse(token);
     }
 
-    public User registerUser(String fullname, String email, String password, Role role) {
+    public User registerUser(
+            String fullname,
+            String email,
+            String password,
+            Role role) {
+
         Optional<User> existingUsers = userRepository.findByEmail(email);
+
         if (existingUsers.isPresent()) {
             throw new EmailAlreadyExistsException("Email already exists!");
         }
 
         String hashedPassword = passwordEncoder.encode(password);
 
-        User newUser = new User(fullname, email, hashedPassword, role);
+        if (role == Role.COMPANY) {
+
+            Company company = new Company();
+
+            company.setFullname(fullname);
+            company.setEmail(email);
+            company.setPassword(hashedPassword);
+            company.setRole(role);
+
+            return companyRepository.save(company);
+        }
+
+        User newUser = new User(
+                fullname,
+                email,
+                hashedPassword,
+                role);
+
         return userRepository.save(newUser);
     }
 
